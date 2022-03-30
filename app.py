d134d0056c2377c4a5770cc6fcb1082c1c21d706
Mohammad Hossein Arab <mohammadhosseinarab14@gmail.com>
2022-03-30 11:24:04 +0430

    Make main file with template

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from ctypes.wintypes import MSG
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import requests
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "de16b73459bbfeceb67a01773f6684e7909e14d1ff61e5789a4c40067cb1a33c"
+socketio = SocketIO(app)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@socketio.on("on-led")
+def handle_message(msg):
+    print('Message: ' + msg["data"])
+    r = requests.get('https://n64522.alavischool.ir/')
+    print(r)
+
+
+@socketio.on("off-led")
+def handle_message(msg):
+    print('Message: ' + msg["data"])
+    r = requests.get('https://n64522.alavischool.ir/nofile')
+    print(r)
+
+
+if __name__ == '__main__':
+    socketio.run(app, port=8080, debug=True)
