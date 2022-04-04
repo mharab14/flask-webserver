d9595e24160062435fff0a17cbd6c19489d1c782
hosein movahedi <h.movahedi.m@outlook.com>
2022-04-04 15:52:10 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-import requests
+import requests as req
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "de16b73459bbfeceb67a01773f6684e7909e14d1ff61e5789a4c40067cb1a33c"
@@ -11,31 +11,21 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
-@app.route("/get")
-def get():
-    return render_template("get.html")
+# @app.route("/led")
+# def get():
+#     return render_template("get.html")
 
-@app.route("/temp")
-def temp():
-    request
+# @app.route("/temp")
+# def temp():
+#     request
 
-@socketio.on("on-led-1")
-def handle_message(msg):
-    print('Message: ' + msg["data"])
-    # r = requests.get('https://n64522.alavischool.ir/')
-    # print(r)
-    socketio.emit("led1", {"data": "Led is on"}, broadcast=True)
-    print("send message")
-
-
-@socketio.on("off-led-1")
-def handle_message(msg):
-    print('Message: ' + msg["data"])
-    # r = requests.get('https://n64522.alavischool.ir/nofile')
-    # print(r)
-    socketio.emit("led1", {"data": "Led is off"}, broadcast=True)
-    print("send message")
-
+@socketio.on("led")
+def handle_led(data):
+    led_number = data["led"];
+    led_status = data["status"];
+    params = dict(led_status = led_status)
+    r = req.get("http://192.168.1." + str(led_number) + "/led", params=params)
 
 if __name__ == '__main__':
     socketio.run(app, port=8080, debug=True)
+
