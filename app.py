55bedb0b2f76d96402368a7141f26f14e342976f
Mohmmad Hossein Arab <mohammadhosseinarab14@gmail.com>
2022-05-01 21:06:59 +0430

    Make some rout and function

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 import requests as req
 
+leds = {}
+temp_hum = {}
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "de16b73459bbfeceb67a01773f6684e7909e14d1ff61e5789a4c40067cb1a33c"
-socketio = SocketIO(app)
+soio = SocketIO(app)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/temp", methods=["GET", "POST"])
+def temp():
+    data = request.get_json()
+    soio.emit("temp", data)
 
-# @app.route("/led")
-# def get():
-#     return render_template("get.html")
-
-# @app.route("/temp")
-# def temp():
-#     request
 
 @socketio.on("led")
 def handle_led(data):
     led_number = data["led"]
     led_status = data["status"]
     params = dict(led_status=led_status)
-    r = req.get("http://192.168.1." + str(led_number) + "/led", params=params)
-
+    req_led = req.get("http://192.168.1" + "." + str(led_number) + "/led", params=params)
+    print("code status :", req_led)
 
 if __name__ == '__main__':
     socketio.run(app, port=8080, debug=True)
+
